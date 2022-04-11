db7e5643a062cde71f3658fa5ab01cee84c6dcbc
nikhilkhandelwal <nikhilkhandelwal7037@gmail.com>
2022-04-11 18:37:59 -0400

    added new class

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import plotly.express as plot
 
 url="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 data_raw=pd.read_csv(url)
+print(data_raw)
 
 
 data_raw=data_raw.drop(columns=['Lat','Long','Province/State'])
--- main3.py
+++ main3.py
@@ -1,4 +1,6 @@
 import mysql.connector
+import matplotlib as plt
+import plotly.express as plot
 
 # Create the connection object
 myconn = mysql.connector.connect(host="localhost", user="root", passwd="Temp@123", database="test")
@@ -10,12 +12,28 @@ try:
     # Reading the Employee data
     cur.execute("select * from covid")
 
+
     # fetching the rows from the cursor object
     result = cur.fetchall()
     # printing the result
 
-    for x in result:
+    #for x in result:
+        #print(x);
+
+    cur.execute("select Date from covid where country='china'")
+    result1=cur.fetchall()
+
+
+
+
+    for x in result1:
         print(x);
+
+    fig = plot.bar(result1, 'china')
+    fig.show()
+
+
+
 except:
     myconn.rollback()
 
